343934bfddc48617ddb76c4c35351b16409849a0
Sandyterry <sandy_terry@hotmail.fr>
2023-11-13 16:30:25 +0100

    exercjava

---
+++ script.js
@@ -0,0 +1,36 @@
+// let nom = "Terry"
+// let prenom = "Sandy"
+// let age = 39
+// let taille = 165
+
+// console.log(nom,prenom,age + 'ans',taille +'cm')
+
+// Exercice 1
+
+//      age = age + 2
+//      console.log(nom, prenom, age + 'ans', taille + 'cm')
+
+// Exercice 2
+
+//      let tout= `je m'apelle ` + nom  + prenom + " j'ai " + age + " ans" + " je mesure " + taille + " cm"
+//      console.log(tout)
+
+// Exercice 3
+
+// let nom = prompt('Entrer votre nom')
+// let prenom = prompt ('Entrer votre prénom')
+// let age = prompt ('Entrer votre age')
+// let taille= prompt('Entrer votre taille')
+
+// console.log ( nom +  ' ' + prenom +  ' ' + age +  ' ' + taille) 
+
+// Exercice 4
+
+let age
+prompt ('Entrer votre age')
+if (age>=18) {
+    console.log ('je suis majeur')
+}
+else { 
+    console.log ('je ne suis pas majeur')
+}
