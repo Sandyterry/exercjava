7550f9421cebb973d1cbd3eff8583d171cd0c645
Sandyterry <sandy_terry@hotmail.fr>
2023-11-14 11:25:16 +0100

    exojavafini

--- script.js
+++ script.js
@@ -12,8 +12,11 @@
 
 // Exercice 2
 
-//      let tout= `je m'apelle ` + nom  + prenom + " j'ai " + age + " ans" + " je mesure " + taille + " cm"
+//      let tout= `je m'appelle ` + nom  + prenom + " j'ai " + age + " ans" + " je mesure " + taille + " cm"
 //      console.log(tout)
+
+
+//      let phrase = "je m'appelle $(nom) $(prenom). J'ai $(age) ans. je mesure $(taille) cm."
 
 // Exercice 3
 
@@ -26,11 +29,59 @@
 
 // Exercice 4
 
-let age
-prompt ('Entrer votre age')
-if (age>=18) {
-    console.log ('je suis majeur')
-}
-else { 
-    console.log ('je ne suis pas majeur')
-}
+// let age
+// prompt ('Entrer votre age')
+// if (age>=18) {
+//     console.log ('je suis majeur')
+// }
+// else { 
+//     console.log ('je ne suis pas majeur')
+// }
+
+// Exercice 5
+
+// let moyenne1 = 15.4
+// let moyenne2 = 9.2
+// let moyenne3 =16.1
+// let moyenne4 = 12.6
+// let somme = moyenne1 + moyenne2 + moyenne3 + moyenne4
+// let moyennegeneral = somme/4
+
+// console.log(somme)
+
+// console.log(moyennegeneral)
+
+// Exercice 6
+
+// function getRandomInclusive( min ,max) {
+//     min = Math.ceil(0);
+//     max = Math.floor(100);
+//     return Math.floor(Math.random()*(100 )+1) + min;
+// }
+// console.log(getRandomInclusive(0,100))
+
+// Exercice 7
+
+// const h1 = document.createElement("h1")
+// h1.textContent = "Hello World!"
+// h1.classname = "monh1"
+
+// Body.appenchild(h1)
+
+// const paragraphe = document.createElement("p")
+// paragraphe.textContent = "je mappelle sandy terry etcetcetctetcetcetctetcetc";
+
+// paragraphe.id = "presentation";
+
+// body.appenchild(paragraphe)
+// h1.parentNode.insertBefore(paragraphe,h1)
+
+// Exercice 8
+
+// let nombre = prompt( "entrer un nombre")
+// let nombreRandom = Math.floor((Math.random() * nombre) + 1 );
+
+// if(nombre > 0){
+//     console.log(nombreRandom)
+// }
+
